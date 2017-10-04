6c59b571051856a5197e462fdd6c20b6fbd4641c
Nagesh C <nageshc1992@gmail.com>
2017-10-05 00:25:08 +0530

    Adding condition to execute for bochs and qemu

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 
 nasm -f bin boot.asm -o boot.com
 dd if=boot.com of=boot.img bs=512 count=1
-qemu-system-i386 -m 128 boot.img
+
+if [[ $1 == "bochs" ]]
+then
+	bochs -f bochsrc.txt
+else
+	qemu-system-i386 -m 128 boot.img
+fi
